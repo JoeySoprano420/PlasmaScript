49e2de56e7f743ddd18af6012be585661abd7d5b
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-09-19 01:44:37 -0400

    Update LlvmBackend.py

--- LlvmBackend.py
+++ LlvmBackend.py
@@ -41,3 +41,57 @@ class LLVMBackend:
             gv.linkage = 'internal'; gv.global_constant = True; gv.initializer = cstr
             ptr = self.builder.bitcast(gv, ir.IntType(8).as_pointer())
             self.builder.call(self.printf, [ptr, val])
+
+class LLVMBackend:
+    def __init__(self, ast):
+        self.ast = ast
+        self.module = ir.Module(name="plasmascript")
+        self.printf = None
+        self.funcs = {}
+        self.builder = None
+        self.locals = {}
+
+    def build(self):
+        self._declare_printf()
+        for node in self.ast:
+            if isinstance(node, FuncNode):
+                self._declare_func(node)
+        for node in self.ast:
+            if isinstance(node, FuncNode):
+                self._define_func(node)
+        return str(self.module)
+
+    def _declare_printf(self):
+        ty = ir.FunctionType(ir.IntType(32), [ir.PointerType(ir.IntType(8))], var_arg=True)
+        self.printf = ir.Function(self.module, ty, name="printf")
+
+    def _declare_func(self, node):
+        fnty = ir.FunctionType(ir.IntType(32), [ir.IntType(32)]*len(node.params))
+        fn = ir.Function(self.module, fnty, name=node.name)
+        if node.export: fn.attributes.add("dllexport")
+        self.funcs[node.name] = fn
+
+    def _define_func(self, node):
+        fn = self.funcs[node.name]
+        block = fn.append_basic_block("entry")
+        self.builder = ir.IRBuilder(block)
+        self.locals = {}
+        for i, arg in enumerate(fn.args):
+            ptr = self.builder.alloca(ir.IntType(32), name=node.params[i] if i < len(node.params) else f"arg{i}")
+            self.builder.store(arg, ptr)
+            self.locals[node.params[i]] = ptr
+        for stmt in node.body.children:
+            if isinstance(stmt, ReturnNode):
+                val = self._eval_expr(stmt.expr)
+                self.builder.ret(val)
+            elif isinstance(stmt, PrintNode):
+                self._print(stmt.expr)
+        if not block.is_terminated:
+            self.builder.ret(ir.Constant(ir.IntType(32), 0))
+
+    def _eval_expr(self, expr):
+        if isinstance(expr, CallNode):
+            args = [self._eval_expr(a) for a in expr.args]
+            return self.builder.call(self.funcs[expr.name], args)
+        # … (reuse number/string/binop handling from earlier) …
+
